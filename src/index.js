6ee2c2e82aeac681616edf6cb6e2beaa109f161e
Nobuyuki Tanabe <tanabe.1478@gmail.com>
2019-03-28 23:19:32 +0900

    giphyAPIを叩くメソッドの作成

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { render } from "react-dom";
 import App from "./App";
 
 import { createStore } from "redux";
@@ -11,9 +11,14 @@ const store = createStore(rootReducer);
 
 console.log(store.getState());
 
-ReactDOM.render(
+render(
   <Provider store={store}>
     <App />
   </Provider>,
   document.getElementById("root")
 );
+
+import giphyAPI from "./APIs/giphyAPI";
+giphyAPI("cat").then(res => {
+  console.log(res.data);
+});
