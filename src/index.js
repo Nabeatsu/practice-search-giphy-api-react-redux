3f79e6fba219b7e2d6557f3efee915eda78e0319
Nobuyuki Tanabe <tanabe.1478@gmail.com>
2019-03-28 23:22:04 +0900

    redux-thunckを適用

--- src/index.js
+++ src/index.js
@@ -2,12 +2,16 @@ import React from "react";
 import { render } from "react-dom";
 import App from "./App";
 
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
 
 import { Provider } from "react-redux";
 
 import rootReducer from "./reducers";
-const store = createStore(rootReducer);
+
+const middleWares = [thunk];
+
+const store = createStore(rootReducer, applyMiddleware(...middleWares));
 
 console.log(store.getState());
 
