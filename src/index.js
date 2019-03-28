a605bf38c25ff3f5d5000092f5287ca08d4b8c04
Nobuyuki Tanabe <tanabe.1478@gmail.com>
2019-03-28 22:39:32 +0900

    Reducerの作成

--- src/index.js
+++ src/index.js
@@ -6,7 +6,10 @@ import { createStore } from "redux";
 
 import { Provider } from "react-redux";
 
+import rootReducer from "./reducers";
 const store = createStore(rootReducer);
+
+console.log(store.getState());
 
 ReactDOM.render(
   <Provider store={store}>
---
+++ src/reducers/index.js
@@ -0,0 +1,3 @@
+import { combineReducers } from "redux";
+import imageUrls from "./imageUrls";
+export default combineReducers({ imageUrls });
